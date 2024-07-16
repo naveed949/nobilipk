e611254adb224242c444d62b44cec2b59e040f74
naveed949 <naveediqbal949@gmail.com>
2024-07-16 13:14:33 +0500

    chore: Remove Footer component from Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
             {/* <Portfolio /> */}
             {/* <Clients /> */}
             <Cta />
-            <Footer />
+            {/* <Footer /> */}
         </>
 
     )
